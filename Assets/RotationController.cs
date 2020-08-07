1862a332ceaa6f6b5c929d9ebee12e8b8dd0fb69
Evan McCall <e.mccallVR@Gmail.com>
2020-08-07 14:16:57 -0400

    Initial commit

---
+++ Assets/RotationController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+    [SerializeField]
+    private bool canRotate
+    {
+        get
+        {
+            return CanRotate;
+        }
+        set
+        {
+            CanRotate = value;
+            Debug.Log("canrotate is Set to " + canRotate);
+        }
+    }
+    private bool CanRotate;
+    [SerializeField]
+    private float offsetX;
+    public float maxRotationPerFrame = 4;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canRotate)
+        {
+            float toRotateBy = CalculateRotaion();
+            transform.Rotate(0, toRotateBy, 0);
+            if (offsetX == 0)
+                DisableRotation();
+        }
+    }
+
+
+    private float CalculateRotaion()
+    {
+        float returnValue = 0;
+        if (Mathf.Abs(offsetX) >= maxRotationPerFrame)
+        {
+            if(Mathf.Sign(offsetX) == 1)
+            {
+                returnValue = -maxRotationPerFrame;
+                offsetX -= maxRotationPerFrame;
+            }
+            else
+            {
+                returnValue = maxRotationPerFrame;
+                offsetX += maxRotationPerFrame;
+            }
+        }
+        else
+        {
+            returnValue = offsetX;
+            offsetX = 0;
+        }
+        Debug.Log("Return Value Calculate Rotation = " + returnValue);
+
+        return returnValue;
+    }
+
+    public void SetOffsetXTo(float offset)
+    {
+        offsetX = offset;
+        if(offset != 0)
+            canRotate = true; ;
+    }
+
+    public void AddToOffsetX(float amountToAdd)
+    {
+        offsetX += amountToAdd;
+        canRotate = true;
+    }
+
+    public void EnableRotation()
+    {
+        canRotate = true;
+    }
+
+    private void DisableRotation()
+    {
+        canRotate = false;
+    }
+
+    public void ResetOffset()
+    {
+        offsetX = 0;
+        canRotate = false;
+    }
+
+}
---
+++ Assets/Scripts/RotateAvatarInputController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAvatarInputController : MonoBehaviour
+{
+    public float speed = 0.1f;
+    Touch cachedTouch;
+    public Vector3 initialPositionCache;
+    public RotationController rotationController;
+    public bool isInitialPositionSet = false;
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if Initial Position is Set
+        //No
+        //Set Start Touch Position and wait until next
+        //Yes
+        //Calculate Difference in new position and previous position
+        //Add Difference to rotation
+        //Expansion - Seperate Rotation object and touch calculation into seperate scripts
+        //Expansion - Add maximum Rotation per frame value - add canRotate Bool - OnTouchEnd Rotate to Finish - On Touch Start Reset amount to rotate by to 0
+        if (!isInitialPositionSet)
+        {
+            //Check if Mouse or Touch is Down
+#if UNITY_EDITOR
+            if(Input.GetMouseButtonDown(0))
+                SetInitialPositionTo(Input.mousePosition);
+#endif
+#if UNITY_ANDROID
+            //TODO When select a button disable RotateAvatar
+            if (Input.touchCount > 0)
+            {
+                SetInitialPositionTo(Input.GetTouch(0).position);
+            }
+#endif
+        }
+        else
+        {
+    #if UNITY_EDITOR
+            if (Input.GetMouseButton(0))
+            {
+                IncreaseOffsetXBy(initialPositionCache , Input.mousePosition);
+            }
+            if(Input.GetMouseButtonUp(0))
+            {
+                ResetInitialPosition();
+            }
+            else
+                initialPositionCache = Input.mousePosition;
+#endif
+#if UNITY_ANDROID
+            if (Input.touchCount > 0)
+            {
+                cachedTouch = Input.GetTouch(0);
+                IncreaseOffsetXBy(initialPositionCache , cachedTouch.position);
+                if(cachedTouch.phase == TouchPhase.Ended || cachedTouch.phase == TouchPhase.Canceled)
+                {
+                    ResetInitialPosition();
+                }
+                else
+                    initialPositionCache = cachedTouch.position;
+            }
+        }
+#endif
+        
+    }
+
+    private void IncreaseOffsetXBy(Vector3 initialPositionCache, Vector3 currentPosition)
+    {
+        rotationController.AddToOffsetX(currentPosition.x - initialPositionCache.x);
+    }
+
+    private void ResetInitialPosition()
+    {
+        initialPositionCache = Vector3.zero;
+        isInitialPositionSet = false;
+    }
+
+    private void SetInitialPositionTo(Vector3 mousePosition)
+    {
+        initialPositionCache = mousePosition;
+        if (!isInitialPositionSet)
+        {
+            isInitialPositionSet = true;
+            rotationController.ResetOffset();
+            rotationController.EnableRotation();
+        }
+    }
+}
